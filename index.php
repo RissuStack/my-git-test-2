71abe177c823689c6da9fda8aa039dc9cbb23705
RissuStack <muhammadparisrm@gmail.com>
2025-08-01 22:01:38 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -1,12 +1,19 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Coba git remote</title>
 </head>
+
 <body>
     <h1>hello world</h1>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio, culpa! Numquam iste soluta odio fugiat obcaecati. Eaque fuga, esse eveniet repudiandae, tempore libero corporis soluta ab laboriosam quo ullam labore.</p>
 </body>
+
 </html>
